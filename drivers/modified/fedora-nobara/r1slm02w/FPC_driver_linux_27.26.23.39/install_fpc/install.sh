876455330df1be9ff91fb817c5d8be686e11da42
Michael Intriago <michaelintriago@gmail.com>
2026-02-15 20:16:11 -0500

    fix: install.sh

--- drivers/modified/fedora-nobara/r1slm02w/FPC_driver_linux_27.26.23.39/install_fpc/install.sh
+++ drivers/modified/fedora-nobara/r1slm02w/FPC_driver_linux_27.26.23.39/install_fpc/install.sh
@@ -1,6 +1,4 @@
-if grep -qE 'ID=fedora' /etc/os-release; then
+
   sudo cp ./libfpcbep.so /usr/lib64/
   sudo chmod +x /usr/lib64/libfpcbep.so
-else
-  sudo cp ./libfpcbep.so /usr/lib/x86_64-linux-gnu/
-fi;
+
--- drivers/modified/fedora-nobara/r1slm02w/FPC_driver_linux_libfprint/install_libfprint/install.sh
+++ drivers/modified/fedora-nobara/r1slm02w/FPC_driver_linux_libfprint/install_libfprint/install.sh
@@ -1,15 +1,6 @@
-if grep -qE 'ID=fedora' /etc/os-release; then
-  sudo dnf -y install libfprint fprintd fprintd-pam 'dnf-command(versionlock)'
-  sudo dnf versionlock libfprint
+  sudo dnf -y install libfprint fprintd fprintd-pam 
   sudo cp -r lib/* /usr/lib/
   sudo cp usr/lib/x86_64-linux-gnu/* /usr/lib64/
   sudo chmod +x /usr/lib64/libfprint-2*
-else
-  sudo cp -r lib/* /lib/
-  sudo cp -r usr/* /usr/
-  sudo mkdir -p /var/log/fpc
-  #avoid libfprint being modified under apt upgrading
-  echo "libfprint-2-2 hold" | sudo dpkg --set-selections
-  sudo chmod 755 /usr/lib/x86_64-linux-gnu/libfprint-2.so.2.0.0
-fi
-echo "Installation completed successfully. You must reboot your system."
+
+
